5d33792a812b1ccc03bf46cbe532f3135d01467d
Mohayemin <mohayeminul.islam@gmail.com>
2025-06-23 17:57:37 -0600

    flask

--- mig_examples/bottle_flask.py
+++ mig_examples/bottle_flask.py
@@ -1,9 +1,9 @@
-from bottle import Bottle
+from flask import Flask
 
 class CQHttp():
   def __init__(self):
     handlers = defaultdict(dict)
-    app = Bottle()
-    app.post('/')(self._handle)
+    app = Flask(__name__)
+    app.route('/', methods=['POST'])(self._handle)
     self._handlers = handlers
     self._app = app
