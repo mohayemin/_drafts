a383b3c7151f9a9d442abbf07556bf333a65a093
Mohayemin <mohayeminul.islam@gmail.com>
2025-06-23 14:41:54 -0600

    jinja

--- mig_examples/cheeta_jinja.py
+++ mig_examples/cheeta_jinja.py
@@ -1,15 +1,15 @@
-from Cheetah import Template
+from jinja2 import Environment, FileSystemLoader
+
 
 def load_config():
   TMPL_PATH = "template/config.txt"
   values = {
     'VALUE': 5,
-    'ROOT': '${ROOT}',
+    'ROOT': '{{ ROOT }}'
   }
-  tmpl_file = open(TMPL_PATH).read()
-  tmpl = Template.Template(
-    tmpl_file, 
-    searchList=[values]
-  )
-  pxe_config = str(tmpl)
-  return pxe_config
+  path, file = os.path.split(TMPL_PATH)
+  loader = loader=FileSystemLoader(path)
+  env = Environment(loader=loader)
+  template = env.get_template(file)
+
+  return template.render(values)
