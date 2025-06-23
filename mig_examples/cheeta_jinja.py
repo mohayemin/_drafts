987772be6657e227134c73ed674fe1f0281428e2
Mohayemin <mohayeminul.islam@gmail.com>
2025-06-23 13:44:13 -0600

    cheeta

--- mig_examples/cheeta_jinja.py
+++ mig_examples/cheeta_jinja.py
@@ -3,7 +3,7 @@ from Cheetah import Template
 
 def load_config():
     pxe_config = str(Template.Template(
-            open("template.txt").read(),
+            open("template/config.txt").read(),
             searchList=[{
                 'VALUE': 5,
                 'ROOT': '${ROOT}',
