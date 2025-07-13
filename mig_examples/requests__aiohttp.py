e77c6a61b097cf58f71537d9c954db5a137497b6
Mohayemin <mohayeminul.islam@gmail.com>
2025-07-13 10:03:47 -0600

    aiohttp

--- mig_examples/requests__aiohttp.py
+++ mig_examples/requests__aiohttp.py
@@ -1,12 +1,15 @@
-import requests
+import aiohttp
 
 url = "..."
 headers = {...}
 
-r = requests.get(url, headers)
-if r.status_code != 200:
-  raise Exception("error getting info")
+async with aiohttp.ClientSession() as ses:
+  async with ses.get(url, headers) as r:
+    if r.status != 200:
+      raise Exception("error getting info")
+    
+    result = await r.json()
+    quota = result["account_quota"]
+    quota_used = result["quota_used"]
 
-result = r.json()
-quota = result["account_quota"]
-quota_used = result["quota_used"]
+    
