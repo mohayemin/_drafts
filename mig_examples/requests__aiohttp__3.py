8c4a3b224fcf6e5bac5394a2d88225355b0c92e2
Mohayemin <mohayeminul.islam@gmail.com>
2025-10-16 10:14:15 -0600

    request_aiohttp 3

---
+++ mig_examples/requests__aiohttp__3.py
@@ -0,0 +1,10 @@
+import requests
+
+def fetch_data(url, params):
+    api_url = API_ROOT + url
+    r = requests.get(api_url, params=params)
+    if r.status_code == 200:
+        return r.json()
+
+    raise Exception("error fetching")
+    
