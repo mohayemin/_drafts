67638fe18e75e6ddb76c1f18705a38cc2e4a4541
Mohayemin <mohayeminul.islam@gmail.com>
2025-10-18 14:24:10 -0600

    update requests

--- mig_examples/requests__aiohttp__3.py
+++ mig_examples/requests__aiohttp__3.py
@@ -1,10 +1,16 @@
-import aiohttp
+import requests
 
-async def fetch_data(url, params):
+API_ROOT = "https://api.example.com/"
+HEADERS = {
+    "User-Agent": "my-app/0.0.1"
+}
+
+def fetch_data(url, params):
   api_url = API_ROOT + url
-  async with aiohttp.ClientSession() as ses:
-    async with ses.get(api_url, params=params) as r:
-      if r.status != 200: 
-        raise Exception("error fetching")
-
-      return await r.json()
+  r = requests.get(api_url, params=params, 
+                   headers=HEADERS)
+  
+  if r.status_code != 200:
+    raise Exception("error fetching")
+  
+  return r.json()
