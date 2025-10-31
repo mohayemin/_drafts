11991cfa044586c1a4f9ee9284ad0025a507ce44
Mohayemin <mohayeminul.islam@gmail.com>
2025-10-31 11:00:47 -0600

    requests

--- mig_examples/requests__aiohttp__4.py
+++ mig_examples/requests__aiohttp__4.py
@@ -30,7 +30,7 @@ from bs4 import BeautifulSoup
 
 if re.search(regex, url):
   result = regex.search(url)
-
+  url = result.group(0)
 
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
